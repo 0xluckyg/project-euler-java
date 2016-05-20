3e43744d9ffcbf2c07419c88d1d9da4019e1ee8d
scottsgcho <scottsgcho@gmail.com>
2016-05-20 13:38:30 -0400

    problem2

---
+++ problem2/src/problem2/problem2.java
@@ -0,0 +1,29 @@
+package problem2;
+
+public class problem2 {
+	public static void main(String[] args) {
+		long fib1 = 1;
+		long fib2 = 2;
+		long fib3 = 0;
+		long total = 0;
+
+		while (total < 4000000) {
+			fib3 = fib1 + fib2;
+		
+			if (fib1 < fib2) {
+				fib1 = fib3;
+			} else {
+				fib2 = fib3;
+			}
+			
+			if (fib3 % 2 == 0) {			
+				total = total + fib3;
+			}
+			
+			fib3 = 0;
+		}
+
+		System.out.println(total + 2);
+
+	}
+}
