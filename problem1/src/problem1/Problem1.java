379bf2ca465aff56055811bb6b0c10333862c0b4
scottsgcho <scottsgcho@gmail.com>
2016-05-20 12:45:00 -0400

    problem1

---
+++ problem1/src/problem1/Problem1.java
@@ -0,0 +1,17 @@
+package problem1;
+
+public class Problem1 {
+		
+	public static void main(String[] args) {
+		int x = 0;
+
+		for (int i = 0; i < 1000; i++) {
+			if ((i % 3 == 0) || (i % 5 == 0)) {
+				x += i;
+			}
+		}
+		
+		System.out.println(x);
+		
+	}
+}
